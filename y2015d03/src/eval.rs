6ed1184e50c296f006bbb82c69c683d3937f8563
Lucille L. Blumire <lucy@llblumire.co.uk>
2020-12-08 21:50:34 +0000

    Cargo fmt

--- y2015d03/src/eval.rs
+++ y2015d03/src/eval.rs
@@ -1,17 +1,18 @@
 use crate::*;
-use std::collections::{BTreeMap, BTreeSet};
+use std::collections::{BTreeMap,
+                       BTreeSet};
 
 #[derive(Debug)]
 enum Direction {
     North,
     East,
     South,
-    West
+    West,
 }
 
 #[derive(Debug)]
 pub struct ParsedInput {
-    directions: Vec<Direction>
+    directions: Vec<Direction>,
 }
 pub fn parse(input: &str) -> IResult<&str, ParsedInput> {
     let north = map(char('^'), |_| Direction::North);
@@ -31,16 +32,16 @@ pub struct SantaSimulator {
 impl SantaSimulator {
     fn do_move(&mut self, direction: &Direction) {
         self.position = match (direction, self.position) {
-            (Direction::North, (x, y)) => (x, y-1),
-            (Direction::East, (x, y)) => (x+1, y),
-            (Direction::South, (x, y)) => (x, y+1),
-            (Direction::West, (x, y)) => (x-1, y),
+            (Direction::North, (x, y)) => (x, y - 1),
+            (Direction::East, (x, y)) => (x + 1, y),
+            (Direction::South, (x, y)) => (x, y + 1),
+            (Direction::West, (x, y)) => (x - 1, y),
         }
     }
     fn do_gift(&mut self) {
         *self.presents.entry(self.position).or_insert(0) += 1;
     }
-    fn simulate<'a, I: Iterator<Item=&'a Direction>>(directions: I) -> SantaSimulator {
+    fn simulate<'a, I: Iterator<Item = &'a Direction>>(directions: I) -> SantaSimulator {
         let mut sim = SantaSimulator::default();
         sim.do_gift();
         for direction in directions {
@@ -54,14 +55,14 @@ impl SantaSimulator {
 pub type Task1 = usize;
 pub type Task2 = usize;
 pub fn compute(input: ParsedInput) -> Result<Output> {
-    Ok(Output { 
-        task1: SantaSimulator::simulate(input.directions.iter()).presents.iter().count(), 
+    Ok(Output {
+        task1: SantaSimulator::simulate(input.directions.iter()).presents.iter().count(),
         task2: {
             let santa = SantaSimulator::simulate(input.directions.iter().step_by(2));
             let robot = SantaSimulator::simulate(input.directions.iter().skip(1).step_by(2));
             let santa_visits: BTreeSet<&(i64, i64)> = santa.presents.keys().collect();
             let robot_visits: BTreeSet<&(i64, i64)> = robot.presents.keys().collect();
-            santa_visits.union(&robot_visits).count()            
-        }
+            santa_visits.union(&robot_visits).count()
+        },
     })
 }
--- y2015d03/src/lib.rs
+++ y2015d03/src/lib.rs
@@ -1,28 +1,24 @@
 pub use common::*;
 
 mod eval;
-pub use eval::ParsedInput;
-pub use eval::parse;
-pub use eval::compute;
-pub use eval::Task1;
-pub use eval::Task2;
+pub use eval::{compute,
+               parse,
+               ParsedInput,
+               Task1,
+               Task2};
 
 impl TryFrom<&str> for ParsedInput {
     type Error = Error;
     fn try_from(input: &str) -> Result<ParsedInput> {
-        Ok(parse(input)
-            .finish()
-            .map(|(_, p)| p)
-            .map_err(|e| nom::error::Error::new(e.input.to_string(), e.code))?)
+        Ok(parse(input).finish().map(|(_, p)| p).map_err(|e| nom::error::Error::new(e.input.to_string(), e.code))?)
     }
 }
-
 
 #[derive(Debug)]
 pub struct Output {
     pub task1: Task1,
-    pub task2: Task2
- }
+    pub task2: Task2,
+}
 impl TryFrom<ParsedInput> for Output {
     type Error = Error;
     fn try_from(parsed_input: ParsedInput) -> Result<Output> {
