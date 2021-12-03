d8b884b649d1823c9ffb292d6f434efbe2c7dc60
Lucille L. Blumire <lucy@llblumire.co.uk>
2021-12-03 17:33:42 +0000

    Simplify day 3

--- src/y2021d03.rs
+++ src/y2021d03.rs
@@ -23,15 +23,16 @@ fn parse(input: &str) -> ParseResult<ParsedInput> {
     parse(input)
 }
 
-fn find_target(input: &[u32], width: u32, predicate: impl Fn(usize) -> bool) -> u32 {
+fn count_bits_at(input: &[u32], bit: u32) -> usize {
+    input
+        .iter()
+        .filter(|number| (*number >> bit) & 1 == 1)
+        .count()
+}
+
+fn find_gamma(input: &[u32], width: u32) -> u32 {
     (0..width).rev().fold(0, |out, bit| {
-        if predicate(
-            input
-                .iter()
-                .filter(|number| ({ (*number >> bit) & (2_u32.pow(1) - 1) }) == 1)
-                .count()
-                * 2,
-        ) {
+        if count_bits_at(input, bit) * 2 >= input.len() {
             (out | 1) << 1
         } else {
             out << 1
@@ -39,54 +40,53 @@ fn find_target(input: &[u32], width: u32, predicate: impl Fn(usize) -> bool) -> 
     }) >> 1
 }
 
-fn find_gamma(input: &[u32], width: u32) -> u32 {
-    let len = input.len();
-    find_target(input, width, |n| n >= len)
-}
-
-fn find_epsilon(input: &[u32], width: u32) -> u32 {
-    let len = input.len();
-    find_target(input, width, |n| n <= len)
+fn find_epsilon_from_gamma(gamma: u32, width: u32) -> u32 {
+    (!gamma) & (2_u32.pow(width) - 1)
 }
 
 fn task1(input: &ParsedInput) -> Result<u32> {
     let gamma = find_gamma(&input.data, input.width);
-    let epsilon = find_epsilon(&input.data, input.width);
+    let epsilon = find_epsilon_from_gamma(gamma, input.width);
     Ok(gamma * epsilon)
 }
 
-fn find_rating_with_target(
-    input: &[u32],
-    width: u32,
-    target: fn(&[u32], u32) -> u32,
-    bit_care: u32,
-) -> Option<u32> {
-    if input.len() <= 1 {
-        return input.first().copied();
-    }
-    let bit_care = bit_care - 1;
-    let target_value = target(input, width);
-    let next_inputs = input
+fn filter_bits(source: &[u32], bit: u32, matches: u32) -> Vec<u32> {
+    source
         .iter()
-        .cloned()
-        .filter(|data| (target_value >> bit_care) & 1 == (data >> bit_care) & 1)
-        .collect::<Vec<_>>();
-    find_rating_with_target(&next_inputs, width, target, bit_care)
+        .copied()
+        .filter(|&data| (data >> bit) & 1 == matches)
+        .collect()
 }
 
-fn find_oxygen_rating(input: &[u32], width: u32, bit_care: u32) -> Option<u32> {
-    find_rating_with_target(input, width, find_gamma, bit_care)
-}
+fn find_ratings(oxygen: &[u32], co2: &[u32], width: u32, bit: u32) -> Option<(u32, u32)> {
+    if bit == 0 || (oxygen.len() == 1 && co2.len() == 1) {
+        return Some((*oxygen.first()?, *co2.first()?));
+    }
+    let bit = bit - 1;
 
-fn find_co2_rating(input: &[u32], width: u32, bit_care: u32) -> Option<u32> {
-    find_rating_with_target(input, width, find_epsilon, bit_care)
+    let oxygen_bit_target = count_bits_at(&oxygen, bit) * 2 >= oxygen.len();
+    let oxygen_bit_target = if oxygen_bit_target { 1 } else { 0 };
+    let next_oxygen = Some(())
+        .filter(|_| oxygen.len() > 1)
+        .map(|_| filter_bits(oxygen, bit, oxygen_bit_target));
+
+    let co2_bit_target = count_bits_at(&co2, bit) * 2 < co2.len();
+    let co2_bit_target = if co2_bit_target { 1 } else { 0 };
+    let next_co2 = Some(())
+        .filter(|_| co2.len() > 1)
+        .map(|_| filter_bits(co2, bit, co2_bit_target));
+
+    find_ratings(
+        next_oxygen.as_ref().map(Vec::as_slice).unwrap_or(oxygen),
+        next_co2.as_ref().map(Vec::as_slice).unwrap_or(co2),
+        width,
+        bit,
+    )
 }
 
 fn task2(input: &ParsedInput) -> Result<u32> {
-    let oxygen = find_oxygen_rating(&input.data, input.width, input.width)
-        .ok_or("Cannot find oxygen rating")?;
-    let co2 = find_co2_rating(&input.data, input.width, input.width - 1)
-        .ok_or("Cannot find co2 rating")?;
+    let (oxygen, co2) = find_ratings(&input.data, &input.data, input.width, input.width)
+        .ok_or("No ratings found")?;
     Ok(oxygen * co2)
 }
 
